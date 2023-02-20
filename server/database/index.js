c9a539aac1e2663f04d4b9dd3b901ca45e347c8d
harunkusnaedi <harunkusnaedi@gmail.com>
2023-02-20 15:54:25 +0700

    tambahkan migration dan seeders

--- server/database/index.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require("sequelize")
-const { DB, USER, PASSWORD, HOST, DIALECT } = require("../config/database.config")
-
-const database = new Sequelize(DB, USER, PASSWORD, {
-    host: HOST,
-    dialect: DIALECT
-})
-
-module.exports = database
